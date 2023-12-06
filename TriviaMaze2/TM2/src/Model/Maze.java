969a671e21a65494bce3375544852393fb2cc518
Matiullah Jalal <matjalal@uw.edu>
2023-12-05 18:46:01 -0800

    Provided Maze JavaDoc

--- TriviaMaze2/TM2/src/Model/Maze.java
+++ TriviaMaze2/TM2/src/Model/Maze.java
@@ -1,6 +1,6 @@
 /*
     This is the Maze class
-    Name: Matiullah Jalal
+    Name: Zakirye Luqman, Hawo Issa, Matiullah Jalal
     Date: 10/25/2023
     Quarter: Autumn 2023
  */
@@ -12,16 +12,32 @@ import java.util.ArrayList;
 
 import Model.QuestionAnswer1;
 
+/**
+ * The maze class shows a maze with doors and rooms.
+ * Each room in the maze has door, and these
+ * doors can be accessed through specific questions.
+ */
 public class Maze implements Serializable {
+    /** Holds 2D grid of the maze. */
     private Room[][] myMaze;
+    /** Holds current room. */
     private Room myCurrentRoom;
+    /** Holds if the game is already started.*/
     private boolean myIsGameStarted;
+    /** Holds time start.*/
     private long myStartTime;
+    /** Holds time end.*/
     private long myEndTime;
+    /** Holds the x-coordinate.*/
     private int myX;
+    /** Holds the y-coordinate.*/
     private int myY;
+    /** Holds the question list .*/
     private ArrayList<QuestionAnswer1> questionList;
 
+    /**
+     * A default constructor to set a 4x4 maze.
+     */
     public Maze() {
         myX = 0;
         myY = 0;
@@ -44,6 +60,12 @@ public class Maze implements Serializable {
         myCurrentRoom = myMaze[0][0];
     }
 
+    /**
+     * A constructor to initialize the fields.
+     * @param maze is the maze.
+     * @param currentX is the x-coordinate.
+     * @param currentY is the y-coordinate.
+     */
     public Maze(Room[][] maze, int currentX, int currentY) {
         myMaze = maze;
         myX = currentX;
@@ -51,11 +73,17 @@ public class Maze implements Serializable {
         myCurrentRoom = myMaze[myX][myY];
     }
 
+    /**
+     * A getter to get current question.
+     * @return returns the question.
+     */
     public QuestionAnswer1 getCurrentQuestion() {
         return myCurrentRoom.getQuestionAnswer();
     }
-
-    // Add this method to check the player's answer and update the game state
+    /**
+     * A method to check the player's answer and update the game state.
+     * @return returns ture if answer is correct otherwise false.
+     */
     public boolean answerQuestion(String playerAnswer) {
         QuestionAnswer1 currentQuestion = getCurrentQuestion();
 
@@ -68,11 +96,17 @@ public class Maze implements Serializable {
         }
     }
 
+    /**
+     * Sets the game to start.
+     */
     public void startGame() {
         myIsGameStarted = true;
         myStartTime = System.currentTimeMillis();
     }
 
+    /**
+     * Sets the game to reset.
+     */
     public void resetGame() {
         myIsGameStarted = false;
         myEndTime = System.currentTimeMillis();
@@ -83,6 +117,10 @@ public class Maze implements Serializable {
         myCurrentRoom = myMaze[0][0];
     }
 
+    /**
+     * Sets the game to save.
+     * @param filePath is the other file.
+     */
     public void saveGame(String filePath) {
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filePath))) {
             out.writeObject(this);
@@ -91,6 +129,11 @@ public class Maze implements Serializable {
         }
     }
 
+    /**
+     * Sets the game to load.
+     * @param filePath is the other file.
+     * @return returns the maze object to deserialize.
+     */
     public static Maze loadGame(String filePath) {
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filePath))) {
             return (Maze) in.readObject();
@@ -100,6 +143,10 @@ public class Maze implements Serializable {
         return null;
     }
 
+    /**
+     * A getter to get total time of game played.
+     * @return returns total time.
+     */
     public long getTotalTime() {
         if (myIsGameStarted) {
             long totalElapsedTime = (myEndTime - myStartTime);
@@ -108,35 +155,52 @@ public class Maze implements Serializable {
         return 0;
     }
 
+    /**
+     * A getter if game is on or not.
+     * @return returns ture if on otherwise false.
+     */
     public boolean isGameOn() {
         return myIsGameStarted;
     }
 
+    /**
+     * A getter to get current room.
+     * @return returns current room.
+     */
     public Room getMyCurrentRoom() {
         return myCurrentRoom;
     }
 
+    /**
+     * A setter to set the entity to move up.
+     */
     public void moveUp() {
         if (myX > 0) {
             myX--;
             myCurrentRoom = myMaze[myX][myY];
         }
     }
-
+    /**
+     * A setter to set the entity to move down.
+     */
     public void moveDown() {
         if (myX < myMaze.length - 1) {
             myX++;
             myCurrentRoom = myMaze[myX][myY];
         }
     }
-
+    /**
+     * A setter to set the entity to move right.
+     */
     public void moveRight() {
         if (myY < myMaze[0].length - 1) {
             myY++;
             myCurrentRoom = myMaze[myX][myY];
         }
     }
-
+    /**
+     * A setter to set the entity to move left.
+     */
     public void moveLeft() {
         if (myY > 0) {
             myY--;
@@ -144,6 +208,10 @@ public class Maze implements Serializable {
         }
     }
 
+    /**
+     * Sets the conditions for game to end.
+     * In order to do so, it will travers in maze.
+     */
     public void isGameOver() {
         char[][] solveMatrix = new char[4][4];
         for (int i = 0; i < 4; i++) {
@@ -175,6 +243,13 @@ public class Maze implements Serializable {
 
     }
 
+    /**
+     * A private method to support the movement of each cell in the maze.
+     * @param maze is the maze.
+     * @param row is the row.
+     * @param col is the column.
+     * @return returns true if movement was successful otherwise false.
+     */
     private static boolean move(char[][] maze, int row, int col) {
         boolean success = false;
 
@@ -199,20 +274,42 @@ public class Maze implements Serializable {
         return success;
     }
 
-
-
+    /**
+     * A method to set the dead end in the maze.
+     * @param maze is the maze.
+     * @param row is the row.
+     * @param col is the column.
+     */
     private static void markDeadEnd(char[][] maze, int row, int col) {
         maze[row][col] = 'd';
     }
 
+    /**
+     * A method to mark the cell that is already visited.
+     * @param maze is the maze.
+     * @param row is the row.
+     * @param col is the column.
+     */
     private static void markVisited(char[][] maze, int row, int col) {
         maze[row][col] = 'v';
     }
-
+    /**
+     * A method to mark the exit points of the maze.
+     * @param maze is the maze.
+     * @param row is the row.
+     * @param col is the column.
+     * @return returns true if at exit otherwise false.
+     */
     private static boolean atExit(char[][] maze, int row, int col) {
         return row == maze.length - 1 && col == maze[row].length - 1;
     }
-
+    /**
+     * A method to check if move is valid or not.
+     * @param maze is the maze.
+     * @param row is the row.
+     * @param col is the column.
+     * @return returns true if move is valid otherwise false.
+     */
     private static boolean validMove(char[][] maze, int row, int col) {
         return row >= 0 && row < maze.length && col >= 0
                 && col < maze[row].length && maze[row][col] == '.';
